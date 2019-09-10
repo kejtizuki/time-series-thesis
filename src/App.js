f5b0a0f720fea917a4f86866939631015ae3f94e
Kasia Zukowska <kzukowska@hotmail.com>
2019-09-10 18:32:05 +0200

    beginning of punchcard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Scatter from './Scatter/Scatter'
-import ScatterMY from './ScatterMY/ScatterMY'
-import './App.scss';
+import ScatterMY from './Scatter/Scatter'
+// import './App.scss';
 
 function App() {
   return (
